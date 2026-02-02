50f6ab952b3a1f8867251c555965bf91aa302e3d
ML-Labib <mahirlabib914.ml@gmail.com>
2026-02-03 05:46:54 +0600

    use beforeEach to remove duplication in tests.

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from "@testing-library/user-event"
 import { Product } from './Product';
@@ -7,8 +7,11 @@ import axios from 'axios';
 vi.mock("axios")
 
 describe("Product component", () => {
-    it("displays the product details correctly", () => {
-        const product = {
+    let product;
+    let loadCart;
+
+    beforeEach(() => {
+        product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -19,8 +22,10 @@ describe("Product component", () => {
             priceCents: 1090,
             keywords: ["socks", "sports", "apparel"]
         };
+        loadCart = vi.fn();
+    })
 
-        const loadCart = vi.fn();
+    it("displays the product details correctly", () => {
 
         render(<Product product={product} loadCart={loadCart} />);
 
@@ -40,27 +45,13 @@ describe("Product component", () => {
             screen.getByTestId("product-rating-stars-image")
         ).toHaveAttribute("src", "images/ratings/rating-45.png");
 
-         expect(
+        expect(
             screen.getByText("87")
         ).toBeInTheDocument();
 
     });
 
     it("add a product to the cart", async () => {
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        };
-
-        const loadCart = vi.fn();
-
 
         render(<Product product={product} loadCart={loadCart} />);
 
@@ -70,10 +61,10 @@ describe("Product component", () => {
 
         expect(axios.post).toHaveBeenCalledWith(
             "/api/cart-items",
-        {
-            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 1
-        })
+            {
+                productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 1
+            })
         expect(loadCart).toHaveBeenCalled();
     });
 });
